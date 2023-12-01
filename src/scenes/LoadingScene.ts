cbccbc120bcc0cb9ff884fcd4163cb46dceb549b
damirsehic <dsehic@gmail.com>
2023-12-01 23:39:30 +0100

    refactor: Preload assets in the LoadingScene

--- src/scenes/LoadingScene.ts
+++ src/scenes/LoadingScene.ts
@@ -1,19 +1,35 @@
 import Phaser from 'phaser'
-import { timeout } from '../util/timeout'
 
 export class LoadingScene extends Phaser.Scene {
   constructor() {
     super('LoadingScene')
   }
 
-  async create() {
-    this.add.text(100, 100, 'Loading...', {
-      fontSize: '16px',
-      color: '#fff',
+  preload() {
+    const windowWidthCenter = Number(this.game.config.width) / 2
+    const windowHeightCenter = Number(this.game.config.height) / 2
+    const loadingText = this.add.text(
+      windowWidthCenter,
+      windowHeightCenter,
+      'Loading...',
+      {
+        fontSize: '16px',
+        color: '#fff',
+      }
+    )
+    loadingText.setOrigin(0.5, 0.5)
+
+    this.load.image('sky', './assets/sky.png')
+    this.load.image('ground', './assets/platform.png')
+    this.load.image('star', './assets/star.png')
+    this.load.image('bomb', './assets/bomb.png')
+    this.load.spritesheet('dude', './assets/dude.png', {
+      frameWidth: 32,
+      frameHeight: 48,
     })
+  }
 
-    await timeout(5000)
-
+  async create() {
     this.scene.start('Scene01')
   }
 }
--- src/scenes/Scene01.ts
+++ src/scenes/Scene01.ts
@@ -15,16 +15,7 @@ export class Scene01 extends Phaser.Scene {
     super('Scene01')
   }
 
-  preload() {
-    this.load.image('sky', './assets/sky.png')
-    this.load.image('ground', './assets/platform.png')
-    this.load.image('star', './assets/star.png')
-    this.load.image('bomb', './assets/bomb.png')
-    this.load.spritesheet('dude', './assets/dude.png', {
-      frameWidth: 32,
-      frameHeight: 48,
-    })
-  }
+  preload() {}
 
   create() {
     this.add.image(400, 300, 'sky')
